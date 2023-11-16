b8aa4d8c7d109b3fff3b7a9940e6be01e779c39c
quanton2003 <quanvyyyb@gmail.com>
2023-11-16 18:33:49 +0700

    csdl

--- duan1.sql
+++ duan1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 14, 2023 lúc 01:12 PM
+-- Thời gian đã tạo: Th10 16, 2023 lúc 12:31 PM
 -- Phiên bản máy phục vụ: 10.4.28-MariaDB
 -- Phiên bản PHP: 8.2.4
 
@@ -34,6 +34,13 @@ CREATE TABLE `baiviet` (
   `trangthai` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Đang đổ dữ liệu cho bảng `baiviet`
+--
+
+INSERT INTO `baiviet` (`id`, `noidung`, `img`, `trangthai`) VALUES
+(1, 'BASAS MONO BLACK NE - LOW TOP - ALL BLACK\r\nMã sản phẩm: AV00001\r\n490.000 VND\r\nNâng cấp chất liệu vải mới bền màu ổn định, kết hợp cùng vẻ ngoài ton sur ton từ trên xuống dưới cùng sắc đen cá tính, giúp phiên bản Basas Mono Black NE trở nên quyến rũ và tiện dụng hơn bao giờ hết. Đây hứa hẹn sẽ là sản phẩm lọt vào danh sách cho những tín đồ thường coi màu đen là sự ưu tiên.', 'sp3_bienthe2', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -47,6 +54,13 @@ CREATE TABLE `banner` (
   `trangthai` tinyint(4) NOT NULL DEFAULT 0,
   `link` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `banner`
+--
+
+INSERT INTO `banner` (`id`, `name`, `img`, `trangthai`, `link`) VALUES
+(1, 'BANNER 1', 'sp1_bienthe1.jpg', 0, '#');
 
 -- --------------------------------------------------------
 
@@ -90,6 +104,16 @@ CREATE TABLE `danhmuc` (
   `name` varchar(200) NOT NULL,
   `mota` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `danhmuc`
+--
+
+INSERT INTO `danhmuc` (`id`, `name`, `mota`) VALUES
+(1, 'Basas', NULL),
+(2, 'Vintas', NULL),
+(3, 'Urbas', NULL),
+(4, 'Pattas', NULL);
 
 -- --------------------------------------------------------
 
@@ -154,6 +178,13 @@ CREATE TABLE `khuyenmai` (
   `trangthai` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Đang đổ dữ liệu cho bảng `khuyenmai`
+--
+
+INSERT INTO `khuyenmai` (`id`, `voucher`, `mota`, `giatri`, `trangthai`) VALUES
+(1, 'HJASHG', 'GIẢM 30K', 30, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -178,6 +209,15 @@ CREATE TABLE `role` (
   `chuc_vu` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Đang đổ dữ liệu cho bảng `role`
+--
+
+INSERT INTO `role` (`id`, `chuc_vu`) VALUES
+(1, 'Khách Hàng'),
+(2, 'Admin'),
+(3, 'Nhân Viên');
+
 -- --------------------------------------------------------
 
 --
@@ -190,8 +230,23 @@ CREATE TABLE `sanpham` (
   `view` int(20) DEFAULT NULL,
   `iddm` int(6) NOT NULL,
   `tinhtrang` tinyint(1) DEFAULT 0,
-  `mota` text DEFAULT NULL
+  `mota` text DEFAULT NULL,
+  `img` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `sanpham`
+--
+
+INSERT INTO `sanpham` (`id`, `name`, `view`, `iddm`, `tinhtrang`, `mota`, `img`) VALUES
+(1, 'BASAS WORKADAY - LOW TOP - BLACK', 2, 1, 0, 'Gender: Unisex\r\nSize run: 35 – 46\r\nUpper: Canvas NE\r\nOutsole: Rubber', 'sp1_nau.jpg'),
+(2, 'BASAS WORKADAY - LOW TOP - REAL TEAL', 10, 1, 0, 'Gender: Unisex\r\nSize run: 35 – 46\r\nUpper: Canvas NE\r\nOutsole: Rubber', 'sp2_den.jpg'),
+(3, 'BASAS BUMPER GUM NE - LOW TOP - BLACK/GUM', 0, 2, 0, 'ánh dấu một bước trưởng thành nữa, Basas Bumper Gum NE (New Episode) ra đời với những cải tiến nhẹ nhàng nhưng đủ tạo được sự thay đổi trong cảm nhận khi trải nghiệm. Vẫn giữ ngoại hình gần như không thay để phát huy đặc tính ứng dụng cao của dòng Basas vốn đã được chứng minh, phần đế màu Gum thú vị và /Foxing thân/ mới làm nền cho phần chất liệu Upper được nâng cấp. Đây được xem là một trong những phiên bản được chúng tôi kì vọng có thể bền vững vượt qua thời gian và không gian, chắc chắn đáng để thử.\r\n', 'sp3_xanh.jpg'),
+(4, 'TRACK 6 SUEDE MOONPHASE - LOW TOP - FOSSIL', 12, 4, 0, 'Dựa trên cảm hứng từ việc tái hiện những sắc xám (Grey) khác nhau hoà cùng những trạng thái ánh sáng trên bề mặt mặt trăng, Ananas Track 6 Suede Moonphase Pack sử dụng chất liệu da lộn (suede) đặc trưng, được phủ toàn bộ thân giày với tông màu sáng tối sắp xếp hài hoà hợp lý. Đây chắc chắn là một sản phẩm must have với những ai yêu thích chất suede và những gam màu Grey trung tính', 'sp4_den.jpg'),
+(5, 'TRACK 7 SUEDE MOONPHASE - LOW TOP - FOSSIL', NULL, 2, 0, 'Dựa trên cảm hứng từ việc tái hiện những sắc xám (Grey) khác nhau hoà cùng những trạng thái ánh sáng trên bề mặt mặt trăng, Ananas Track 6 Suede Moonphase Pack sử dụng chất liệu da lộn (suede) đặc trưng, được phủ toàn bộ thân giày với tông màu sáng tối sắp xếp hài hoà hợp lý. Đây chắc chắn là một sản phẩm must have với những ai yêu thích chất suede và những gam màu Grey trung tính', 'sp5_trang.jpg'),
+(8, 'TRACK 8 SUEDE MOONPHASE - LOW TOP - FOSSIL', NULL, 3, 0, 'Dựa trên cảm hứng từ việc tái hiện những sắc xám (Grey) khác nhau hoà cùng những trạng thái ánh sáng trên bề mặt mặt trăng, Ananas Track 6 Suede Moonphase Pack sử dụng chất liệu da lộn (suede) đặc trưng, được phủ toàn bộ thân giày với tông màu sáng tối sắp xếp hài hoà hợp lý. Đây chắc chắn là một sản phẩm must have với những ai yêu thích chất suede và những gam màu Grey trung tính.', 'sp6_trang.jpg'),
+(9, 'TRACK 4 SUEDE MOONPHASE - LOW TOP - FOSSIL', NULL, 3, 0, 'Dựa trên cảm hứng từ việc tái hiện những sắc xám (Grey) khác nhau hoà cùng những trạng thái ánh sáng trên bề mặt mặt trăng, Ananas Track 6 Suede Moonphase Pack sử dụng chất liệu da lộn (suede) đặc trưng, được phủ toàn bộ thân giày với tông màu sáng tối sắp xếp hài hoà hợp lý. Đây chắc chắn là một sản phẩm must have với những ai yêu thích chất suede và những gam màu Grey trung tính.', 'sp7_den.jpg'),
+(10, 'URBAS SC - LOW TOP - FAIR ORCHID', NULL, 4, 0, 'Gender: Unisex\r\nSize run: 35 – 46\r\nUpper: Canvas NE\r\nOutsole: Rubber', 'sp8_trang.jpg');
 
 -- --------------------------------------------------------
 
@@ -207,9 +262,18 @@ CREATE TABLE `user` (
   `diachi` varchar(200) DEFAULT NULL,
   `email` varchar(200) NOT NULL,
   `dienthoai` varchar(20) NOT NULL,
-  `idrole` int(20) NOT NULL DEFAULT 0,
-  `img` int(11) NOT NULL
+  `idrole` int(20) NOT NULL DEFAULT 1,
+  `img` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `user_name`, `diachi`, `email`, `dienthoai`, `idrole`, `img`) VALUES
+(1, 'admin', 'admin', 'admin', NULL, 'quanvyyyb@gmail.com', '0343147165', 2, NULL),
+(2, 'hainam', 'hainam12', 'hải nam', NULL, 'hainam@gmail.com', '092138128', 1, NULL),
+(3, 'yen1212', 'yen1212', 'Nhân Viên Yến', NULL, 'yen1212@gmail.com', '0343147122', 3, NULL);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -309,13 +373,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT cho bảng `baiviet`
 --
 ALTER TABLE `baiviet`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `banner`
 --
 ALTER TABLE `banner`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `bienthesanpham`
@@ -333,7 +397,7 @@ ALTER TABLE `binhluan`
 -- AUTO_INCREMENT cho bảng `danhmuc`
 --
 ALTER TABLE `danhmuc`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT cho bảng `donhang`
@@ -357,7 +421,7 @@ ALTER TABLE `hinhanhbienthe`
 -- AUTO_INCREMENT cho bảng `khuyenmai`
 --
 ALTER TABLE `khuyenmai`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `lienhe`
@@ -369,19 +433,19 @@ ALTER TABLE `lienhe`
 -- AUTO_INCREMENT cho bảng `role`
 --
 ALTER TABLE `role`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT cho bảng `sanpham`
 --
 ALTER TABLE `sanpham`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT cho bảng `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
