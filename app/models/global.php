84689208490d272726d962285bf8acc28302c1ba
quanton2003 <quanvyyyb@gmail.com>
2023-11-16 15:42:52 +0700

    edit global

--- app/models/global.php
+++ app/models/global.php
@@ -1,5 +1,5 @@
 <?php 
 // khai báo đường dẫn
-    define("IMG_PATH_ADMIN","../uploads/");
-    define("IMG_PATH_USER","/uploads/");
+    define("IMG_PATH_ADMIN","../../../public/uploads/");
+    define("IMG_PATH_USER","../../../public/uploads/");
 ?>
