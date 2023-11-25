83b5c36b4bec920e9d410bd4bee97808ba58d55a
quanton2003 <125117562+quanton2003@users.noreply.github.com>
2023-11-25 14:38:03 +0700

    banner

--- app/views/client/header.php
+++ app/views/client/header.php
@@ -78,7 +78,7 @@ $html_showdm = showdm_all(danhmuc_all());
                     <div class="row small-gutters">
                         <div class="col-xl-3 col-lg-3 d-lg-flex align-items-center">
                             <div id="logo">
-                                <a href="index.html"><img src="public/client/img/logo.svg" alt="" width="100" height="35"></a>
+                                <a href="index.php"><img src="public/client/img/logo.svg" alt="" width="100" height="35"></a>
                             </div>
                         </div>
                         <nav class="col-xl-6 col-lg-7">
