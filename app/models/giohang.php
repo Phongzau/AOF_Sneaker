7500c74a207187f53f33e5751d87bf937f510d74
Phongzau <phongzau2004@gmail.com>
2023-11-26 14:15:40 +0700

    update viewcart

--- app/models/giohang.php
+++ app/models/giohang.php
@@ -6,27 +6,41 @@
 //    pdo_execute($sql, $idpro,$price, $name, $img,$soluong,$thanhtien,$idbill);
 // }
 
-// function viewcart(){
-//     $html_cart ='';
-// $i = 1;
-// foreach ($_SESSION['giohang'] as $sp) {
-//     extract($sp);
-//     $tt = $price*$soluong;
-//     $html_cart.='
-//     <tr>
-//     <td>'.$i.'</td>
-//     <td><img src="'.IMG_PATH_USER.$img.'" alt="" style="  width: 100px; " ></td>
-//     <td>'.$name.'</td>
-//     <td>'.$price.'</td>
-//     <td>'.$soluong.'</td>
-//     <td>$'.$tt.'</td>
-//     <td><a href="">Xóa</a></td>
-// </tr>
-//     ';
-//     $i++;
-// }
-// return  $html_cart ;
-// }
+function viewcart(){
+    $html_cart ='';
+    $i = 1;
+    foreach ($_SESSION['giohang'] as $sp) {
+    extract($sp);
+    $tt = $price*$soluong;
+    $html_cart.='   <tr>
+                        <td>'.$i.'</td>
+                        <td><span class="item_cart">'.$name.'</span></td>
+                        <td>
+                            <div class="thumb_cart">
+                                <img width="30%" src="'.IMG_PATH_USER.$img.'" data-src="'.IMG_PATH_USER.$img.'" class="lazy" alt="Image">
+                            </div>
+                        </td>
+                        <td><strong>$'.$price.'</strong></td>
+                        <td><strong>'.$soluong.'</strong></td>
+                        <td><strong>'.$tt.'</strong></td>
+                        <td class="options">
+                            <a href="#"><i class="ti-trash"></i></a>
+                        </td>
+                    </tr>';
+    $i++;
+}
+return  $html_cart ;
+}
+
+function get_tongdonhang() {
+    $tong = 0;
+    foreach($_SESSION['giohang'] as $sp) {
+        extract($sp);
+        $tt = (INT)$price * (INT)$soluong;
+        $tong+=$tt;
+    }
+    return $tong;
+}
 // function get_tongdonhang(){
 //     $tong =0;
 
